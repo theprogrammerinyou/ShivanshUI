10111ff2671735aa12fbc38a7a9472e8001cdbbe
Shivansh shukla <yourdemocracyishacked@gmail.com>
2022-02-08 23:37:53 +0530

    remove logs

--- components/card/card.js
+++ components/card/card.js
@@ -4,7 +4,7 @@ const card = document.querySelector(".dismiss-card");
 const cardActions = document.querySelector(".card-actions-dismiss");
 
 const dismissCard = () => {
-    console.log("clicked");
+    // console.log("clicked");
     card.classList.add("hide");
     cardActions.classList.add("hide");
 }
