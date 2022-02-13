9580039b3103ab875f7dab2c04da2bd0f33520cc
Shivansh Shukla <lordshivansh2303@gmail.com>
2022-02-13 10:11:51 +0530

    change px to rem, cleanup files

--- components/card/card.js
+++ components/card/card.js
@@ -4,7 +4,6 @@ const card = document.querySelector(".dismiss-card");
 const cardActions = document.querySelector(".card-actions-dismiss");
 
 const dismissCard = () => {
-    // console.log("clicked");
     card.classList.add("hide");
     cardActions.classList.add("hide");
 }
